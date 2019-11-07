156b587974881ecbbc5f3f986bb21645fc30e3e5
adityain98 <adityain98@gmail.com>
2019-11-07 19:08:40 +0700

    selesai search dan api

--- routes/imageRoute.js
+++ routes/imageRoute.js
@@ -1,3 +1,9 @@
 const router = require('express').Router()
+const ImageController = require('../controllers/imageController')
+const {authentication} = require('../middlewares/authentication')
+
+// router.use(authentication)
+router.get('/search/:search', ImageController.search)
+router.post('/clarifai', ImageController.clarifai)
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const userRoute = require('./userRoute')
+const imageRoute = require('./imageRoute')
 
 router.use('/users', userRoute)
+router.use('/images', imageRoute)
 
 module.exports = router

a0cba714af73741ba7d3f705cb7b27af51df4b77
akbfitra <akbarfitralistyono@gmail.com>
2019-11-07 20:40:20 +0700

    gcs upload ok

--- routes/imageRoute.js
+++ routes/imageRoute.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 const ImageController = require('../controllers/imageController')
-const {authentication} = require('../middlewares/authentication')
+const multer = require('../middlewares/multer')
+const gcs = require('../middlewares/gcs')
+const { authentication } = require('../middlewares/authentication')
+const { authorization } = require('../middlewares/authentication')
 
-// router.use(authentication)
-router.get('/search/:search', ImageController.search)
-router.post('/clarifai', ImageController.clarifai)
+
+router.post('/', authentication, multer.single('img'), gcs, ImageController.uploadImage)
 
 module.exports = router

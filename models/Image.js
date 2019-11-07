0d2ce48861178e0129be2bf39ec8050d52cd2f02
akbfitra <akbarfitralistyono@gmail.com>
2019-11-07 21:16:55 +0700

    upload ok

--- models/Image.js
+++ models/Image.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const imageSchema = new Schema({
-  user_id: [{ 
+  user_id: { 
     type: Schema.Types.ObjectId, 
     ref: 'User'
-  }],
+  },
   name: {
     type: String
   },

58d768286f3f6efaa90bbd7dd915fb0b76c24fb0
akbfitra <akbarfitralistyono@gmail.com>
2019-11-08 09:17:09 +0700

    delete ok

--- routes/imageRoute.js
+++ routes/imageRoute.js
@@ -1,15 +1,15 @@
 const router = require('express').Router()
 const ImageController = require('../controllers/imageController')
-const {authentication} = require('../middlewares/authentication')
+const { authentication } = require('../middlewares/authentication')
 const { authorization } = require('../middlewares/authentication')
 const gcs = require('../middlewares/gcs')
 const multer = require('../middlewares/multer')
 
-// router.use(authentication)
-// router.get('/search/:search', ImageController.search)
-// router.post('/clarifai', ImageController.clarifai)
+router.get('/', ImageController.findData)
 
+router.get('/user', authentication, ImageController.getAllImageUser)
 router.post('/', authentication, multer.single('img'), gcs, ImageController.uploadImage)
+router.delete('/:id', authentication, authorization, ImageController.deleteImage)
 
 
 module.exports = router
